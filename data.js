fa8914a75062ac76b94b65ddad287125c4a65a2f
rounak3dgraphicdesigner-bit <rounak.3dgraphicdesigner@gmail.com>
2026-07-17 11:50:58 +0530

    t
    start

---
+++ data.js
@@ -0,0 +1,162 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-00000",
+      "name": "00000",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -1.3369924040703616,
+        "pitch": -0.013392351289123638,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.614034589613187,
+          "pitch": 0.05655324061198819,
+          "rotation": 6.283185307179586,
+          "target": "1-01"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.8088156397956316,
+          "pitch": 0.17406224788485858,
+          "title": "Sofa",
+          "text": "A sofa is a comfortable piece of furniture designed for sitting, relaxing, and socializing. It is commonly placed in living rooms, offices, lounges, and entertainment areas. Sofas are available in various designs, sizes, materials, and colors to match different interior styles. Modern sofas often feature soft cushions, durable fabrics, leather finishes, and stylish shapes. They provide comfort while enhancing the beauty and functionality of a space."
+        }
+      ]
+    },
+    {
+      "id": "1-01",
+      "name": "01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14970948700629272,
+          "pitch": 0.08482647586656711,
+          "rotation": 0,
+          "target": "2-02"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.425170630879638,
+          "pitch": 0.054772630213879125,
+          "title": "Bar Area",
+          "text": "A sofa is a comfortable piece of furniture designed for sitting, relaxing, and socializing. It is commonly placed in living rooms, offices, lounges, and entertainment areas. Sofas are available in various designs, sizes, materials, and colors to match different interior styles. Modern so"
+        }
+      ]
+    },
+    {
+      "id": "2-02",
+      "name": "02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2667365798781844,
+          "pitch": 0.09386149803106036,
+          "rotation": 0,
+          "target": "3-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-03",
+      "name": "03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
